fc1569a0d795ffc78e0b5f1ee0745a5fbb1dbcba
iyasuwatts <iyasuwatts@iyasuwattss-MacBook-Pro.local>
2017-06-23 14:08:40 -0400

    Created WeatherFragment

--- app/src/main/java/com/zipcoder/androidweatherapplication/MainActivity.java
+++ app/src/main/java/com/zipcoder/androidweatherapplication/MainActivity.java
@@ -9,10 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        update();
     }
 
-    private void update(Bundle savedInstanceSate){
+    private void update(){
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.weather, new WeatherFragment())
                 .commit();
--- app/src/main/java/com/zipcoder/androidweatherapplication/WeatherFragment.java
+++ app/src/main/java/com/zipcoder/androidweatherapplication/WeatherFragment.java
@@ -1,24 +1,13 @@
 package com.zipcoder.androidweatherapplication;
 
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 
 
 /**
@@ -26,16 +15,6 @@ import java.util.Locale;
  */
 public class WeatherFragment extends Fragment implements View.OnClickListener {
 
-    TextView cityField;
-    TextView updatedField;
-    TextView detailsField;
-    TextView currentTempField;
-    TextView weatherIcon;
-    Typeface weatherFont;
-    ListView listView;
-    SearchView searchView;
-
-    String selectedCity = "wilmington";
 
     Handler handler;
 
